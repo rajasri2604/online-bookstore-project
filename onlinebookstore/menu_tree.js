24aae89262a1efb30d157d7d4285bf89d571d263
rajasri2604 <146611359+rajasri2604@users.noreply.github.com>
2024-10-11 19:30:23 +0530

    Delete onlinebookstore directory

--- onlinebookstore/menu_tree.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-
-/*My account drop-down menu*/
-function myddang_show(elementId){
-	document.getElementById(elementId).style.display="block";
-	}
-function myddang_hidden(elementId){
-	document.getElementById(elementId).style.display="none";
-	}
